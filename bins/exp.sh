422689905904373aacab5da2a6a6fd5a541ccd72
Naoki1101 <naoki.narimatsu111@gmail.com>
2021-04-09 19:49:49 +0900

    [Add] Add exp_003

--- bins/exp.sh
+++ bins/exp.sh
@@ -2,4 +2,5 @@
 
 cd ../
 # python -m experiments.exp_001 -m -c "First Submit"
-python -m experiments.exp_002 -m -c "legacy_seresnext50_32x4d"
+# python -m experiments.exp_002 -m -c "legacy_seresnext50_32x4d"
+python -m experiments.exp_003 -m -c "th=0.7 -> 0.65"
