29418a8f0d901083ee3bd82eae08caf94665c6d7
Naoki1101 <naoki.narimatsu111@gmail.com>
2021-04-10 20:29:36 +0900

    [Add] Add exp_005

--- bins/exp.sh
+++ bins/exp.sh
@@ -4,4 +4,5 @@ cd ../
 # python -m experiments.exp_001 -m -c "First Submit"
 # python -m experiments.exp_002 -m -c "legacy_seresnext50_32x4d"
 # python -m experiments.exp_003 -m -c "th=0.7 -> 0.65"
-python -m experiments.exp_004 -m -c "use secondary_labels"
+# python -m experiments.exp_004 -m -c "use secondary_labels"
+python -m experiments.exp_005 -m -c "mixup"
