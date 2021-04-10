42b27be15591e83da50b5ed96d97ba3be0494b0a
Naoki1101 <naoki.narimatsu111@gmail.com>
2021-04-11 08:50:57 +0900

    [Add] Add exp_006

--- bins/exp.sh
+++ bins/exp.sh
@@ -5,4 +5,5 @@ cd ../
 # python -m experiments.exp_002 -m -c "legacy_seresnext50_32x4d"
 # python -m experiments.exp_003 -m -c "th=0.7 -> 0.65"
 # python -m experiments.exp_004 -m -c "use secondary_labels"
-python -m experiments.exp_005 -m -c "mixup"
+# python -m experiments.exp_005 -m -c "mixup"
+python -m experiments.exp_006 -m -c "train only first 80% audio"
