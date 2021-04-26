a1eb6accd51cfbc32870a03f4a273a3d40526ca0
Naoki1101 <naoki.narimatsu111@gmail.com>
2021-04-26 22:45:24 +0900

    [Add] Add exp_032

--- src/loss.py
+++ src/loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -17,3 +18,20 @@ class BCEWithLogitsLoss(nn.Module):
 
     def forward(self, yhat, y):
         return self.bcel(yhat, y)
+
+
+class BCEFocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def forward(self, preds, targets):
+        bce_loss = nn.BCEWithLogitsLoss(reduction="none")(preds, targets)
+        probas = torch.sigmoid(preds)
+        loss = (
+            targets * self.alpha * (1.0 - probas) ** self.gamma * bce_loss
+            + (1.0 - targets) * probas ** self.gamma * bce_loss
+        )
+        loss = loss.mean()
+        return loss
