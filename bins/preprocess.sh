75ecd49989f9e09a0614a21f727c56929b103ad4
Naoki1101 <naoki.narimatsu111@gmail.com>
2021-05-12 11:24:43 +0900

    [Add] Add exp_083

--- bins/preprocess.sh
+++ bins/preprocess.sh
@@ -2,7 +2,7 @@
 
 cd ../
 # python src/preprocess/01_resample.py
-python src/preprocess/02_extract_soundscape_noise.py
+# python src/preprocess/02_extract_soundscape_noise.py
 # python src/preprocess/03_extract_freesound_noise.py
 # python src/preprocess/04_create_valid_position.py
 # python src/preprocess/05_extract_non_test_region_birds.py
---
+++ experiments/exp_080/__main__.py
@@ -0,0 +1,98 @@
+import argparse
+import datetime
+import logging
+import warnings
+
+from pathlib import Path
+
+from .trainer import NNTrainer
+from .factory import get_drop_idx
+from src import const
+from src.utils import DataHandler, Timer, seed_everything, Notificator  # , Kaggle
+
+warnings.filterwarnings("ignore")
+
+# ===============
+# Settings
+# ===============
+parser = argparse.ArgumentParser()
+parser.add_argument("--notify", default=const.CONFIG_DIR / "notify.yml")
+parser.add_argument("--debug", action="store_true")
+parser.add_argument("-m", "--multigpu", action="store_true")
+parser.add_argument("-c", "--comment")
+options = parser.parse_args()
+
+exp_dir = Path(__file__).resolve().parents[0]
+exp_id = str(exp_dir).split("/")[-1]
+
+dh = DataHandler()
+cfg = dh.load(exp_dir / "config.yml")
+
+notify_params = dh.load(options.notify)
+
+comment = options.comment
+model_name = cfg.model.backbone
+now = datetime.datetime.now()
+run_name = f"{exp_id}_{now:%Y%m%d%H%M%S}"
+
+logger_path = Path(const.LOG_DIR / run_name)
+
+
+# ===============
+# Main
+# ===============
+def main():
+    t = Timer()
+    seed_everything(cfg.seed)
+
+    logger_path.mkdir(exist_ok=True)
+    logging.basicConfig(filename=logger_path / "train.log", level=logging.DEBUG)
+
+    dh.save(logger_path / "config.yml", cfg)
+
+    with t.timer("load data"):
+        train_df = dh.load(const.INPUT_DATA_DIR / "train_metadata.csv")
+        valid_df = dh.load(const.INPUT_DATA_DIR / "train_soundscape_labels.csv")
+
+    with t.timer("preprocess"):
+        train_df["target"] = 1.0
+
+        valid_df["file_id"] = valid_df["audio_id"].astype(str) + "_" + valid_df["site"]
+        valid_df["file_name"] = valid_df["file_id"].map(const.TRAIN_SOUNDSCAPES_CODE)
+        valid_df["target"] = valid_df["birds"].apply(
+            lambda x: 1.0 if x != "nocall" else 0.0
+        )
+
+    with t.timer("drop rows"):
+        if cfg.drop is not None:
+            drop_idx = get_drop_idx(cfg.drop)
+            train_df = train_df.drop(drop_idx, axis=0).reset_index(drop=True)
+
+    with t.timer("train model"):
+        trainer = NNTrainer(cfg, run_name, options.multigpu, options.debug)
+        cv = trainer.train(
+            train_df=train_df,
+            valid_df=valid_df,
+        )
+        trainer.save()
+
+        run_name_cv = f"{run_name}_{cv:.3f}"
+        logger_path.rename(const.LOG_DIR / run_name_cv)
+        logging.disable(logging.FATAL)
+
+    with t.timer("notify"):
+        process_minutes = t.get_processing_time()
+        notificator = Notificator(
+            run_name=run_name_cv,
+            model_name=cfg.model.backbone,
+            cv=round(cv, 4),
+            process_time=round(process_minutes, 2),
+            comment=comment,
+            params=notify_params,
+        )
+        notificator.send_line()
+        notificator.send_notion()
+
+
+if __name__ == "__main__":
+    main()
